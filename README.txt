4e1a36db7a7865c2325d6533f6244ffca21c42b8
samjoski <samjoski@gmail.com>
2016-05-24 14:52:59 -0500

    Wrote CheckWinner, altered main, and logged README

--- README.txt
+++ README.txt
@@ -5,12 +5,13 @@ Completed functions:
 	void Initialize(int Players[]);
 	void main();
 	void TakeTurn(int Players[]);
+	int CheckWinner(int Players[]);
 
 Functions to be written:
-	int CheckWinner(int Players[]);
 	void DeclareWinner(int winner);
 
 Notes:
 	1:52 - Wrote Initialize
 	2:14 - Wrote main
 	2:19 - Wrote TakeTurn, made corrections in main while loop
+	2:51 - Wrote CheckWinner
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 //main.cpp
 const int NUMPLAYERS = 10;
+const int GOAL = 25;
 
 void main() {
 	int Players[NUMPLAYERS];
@@ -8,7 +9,7 @@ void main() {
 	while (true) {
 		TakeTurn(Players);
 		
-		if (CheckWinner(Players) != -1)
+		if (CheckWinner(Players))
 			break;
 	}
 	
@@ -28,4 +29,12 @@ void TakeTurn (int Players[]) {
 		Players[i] += (rand() % 6) + 1;	//6 sided "dice"
 	}
 	return;
+}
+
+bool CheckWinner (int Players[]) {
+	for (int i=0; i<NUMPLAYERS; i++) {
+		if (Player[i] >= GOAL)
+			return true;
+	}
+	return false;
 }
